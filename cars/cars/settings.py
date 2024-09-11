3044092829110ee1de54482cf951d3569e23759f
chekist <georg4932@mail.ru>
2024-09-11 19:24:42 +1000

    Add basic serializers, add basic views, register views in urls, create model Car, migrate, reg Car on admin panel

--- cars/cars/settings.py
+++ cars/cars/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'carsapp',
 ]
 
 MIDDLEWARE = [
@@ -97,7 +98,12 @@ DATABASES = {
         'PORT': '',
     }
 }
-print(DATABASES)
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
--- cars/carsapp/models.py
+++ cars/carsapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Car(models.Model):
+    make = models.CharField(max_length=20, blank=False, null=False)
+    model = models.CharField(max_length=20, blank=False, null=False)
+    # year = models.DateField()
+    year = models.CharField(max_length=4, blank=False, null=False)
+    description = models.CharField(max_length=100, blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.make} {self.model} {self.year}'
