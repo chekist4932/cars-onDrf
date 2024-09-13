28ab0f4d5a0ee4320fd5f289ddbeff4fb04823e2
chekist <georg4932@mail.ru>
2024-09-14 00:22:22 +1000

    serializers.py: CommentSerializer: add HiddenField author; models.py: Comment: set field [car] - nullable=False;

--- cars/carsapp/models.py
+++ cars/carsapp/models.py
@@ -8,7 +8,7 @@ class Car(models.Model):
     make = models.CharField(max_length=20, blank=False, null=False)
     model = models.CharField(max_length=20, blank=False, null=False)
     year = models.CharField(max_length=4, blank=True, null=True)
-    description = models.CharField(max_length=100, blank=False, null=False)
+    description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -20,7 +20,7 @@ class Car(models.Model):
 class Comment(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, null=False)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
--- cars/carsapp/serializers.py
+++ cars/carsapp/serializers.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import Group, User
 from rest_framework import serializers
 
+from .models import Car, Comment
+
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -12,3 +14,19 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ['url', 'name']
+
+
+class CarSerializer(serializers.ModelSerializer):
+    owner = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = Car
+        fields = "__all__"
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = Comment
+        fields = "__all__"
