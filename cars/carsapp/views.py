bb888c53ff8d41280e1de32780ae7045c65a45fa
chekist <georg4932@mail.ru>
2024-09-14 00:19:09 +1000

    refactore class CarViewSet: add action for comments with permissions - get, post;

--- cars/carsapp/views.py
+++ cars/carsapp/views.py
@@ -1,7 +1,12 @@
 from django.contrib.auth.models import Group, User
-from rest_framework import permissions, viewsets
+from rest_framework import permissions, viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import serializers
 
-from cars.carsapp.serializers import GroupSerializer, UserSerializer
+from .serializers import GroupSerializer, UserSerializer, CarSerializer, CommentSerializer
+from .models import Car, Comment
+from .permissions import IsOwnerOrReadOnly, CreateOrReadOnly
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -20,3 +25,29 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all().order_by('name')
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+class CarViewSet(viewsets.ModelViewSet):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    # def get_queryset(self):
+    #     pk = self.kwargs.get("pk")
+    #     return Comment.objects.filter(id=pk)
+
+    # @action(methods=['GET', 'POST'], detail=True, serializer_class=CommentSerializer,
+    @action(methods=['GET'], detail=True, serializer_class=CommentSerializer,
+            permission_classes=[CreateOrReadOnly])
+    def comments(self, request, pk=None):
+        comments = Comment.objects.all() if not pk else Comment.objects.filter(car=pk)
+        comments = CommentSerializer(comments, many=True).data
+        return Response({'comments': comments})
+
+    @comments.mapping.post
+    def add_comment(self, request, pk=None):
+        serializer = CommentSerializer(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'comments': serializer.data})
+        return Response({'error': 'serializer.errors'})
