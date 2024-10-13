d854b7b369033cb732eb08e47b8ee26d5cbf4c5e
chekist <georg4932@mail.ru>
2024-10-13 21:31:37 +1000

    refactore urls.py: del comment lines

--- cars/cars/urls.py
+++ cars/cars/urls.py
@@ -18,16 +18,15 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 
-from tutorial.quickstart import views
+from carsapp.views import UserViewSet, GroupViewSet, CarViewSet, CommentViewSet
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+base_router = routers.DefaultRouter()
+base_router.register(r'cars', CarViewSet, basename='car')
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api/', include(base_router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
